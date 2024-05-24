efb8a334388f34bcd8663e49c17f80a179d0a32f
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-24 16:58:40 +0900

    feat: User 모델 생성

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+// const jwt = require('jsonwebtoken');
+// require('dotenv').config();
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema(
+    {
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        name: { type: String, required: true },
+        level: { type: number, required: true , default: 0},
+        privilege: { type: string, required: true, default: "customer" }, // admin || customer
+    },
+    { timestamps: true }
+)
+
+// 비밀번호 제거 + a
+userSchema.methods.toJSON = function() {
+    const obj = this._doc;
+    delete obj.password
+    delete obj.__v
+}
+
+// // 토큰 생성
+// userSchema.methods.generateToken = function () {
+//     const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, { expiresIn: '1h' });
+//     return token;
+// };
+
+// User 모델 생성
+const User = mongoose.model('User', userSchema);
+module.exports = User;
