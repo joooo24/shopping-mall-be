6510ccb6e1e85471ef67a034ffa1e4855452f68d
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-31 10:33:20 +0900

    feat: Order 모델 수정

--- models/Order.js
+++ models/Order.js
@@ -1,29 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = require('./User');
+const User = require('./user'); // User인데 user로 해야 경로 오류 안나는 이슈
 const Cart = require('./Cart');
 
 const orderSchema = Schema(
     {
         userId: { type: mongoose.ObjectId, ref: User, required: true }, // 주문한 사용자의 ID
+        status: { type: String, default: "preparing" },
+        // status: { // 주문 상태 (보류, 처리 중, 발송됨, 배달됨, 취소됨)
+        //     type: String, 
+        //     required: true, 
+        //     enum: ['pending', 'preparing',  'processing', 'shipped', 'delivered', 'cancelled'], 
+        //     default: 'pending'
+        // },
+        totalPrice: { type: Number, required: true, default: 0 }, // 총 주문 가격
         cartId: { type: mongoose.ObjectId, ref: Cart, required: true }, // 주문에 대한 장바구니 ID
-        totalPrice: { type: Number, required: true }, // 총 주문 가격
-        status: { 
-            type: String, 
-            required: true, 
-            enum: ['pending', 'processing', 'shipped', 'delivered', 'cancelled'], // 주문 상태 (보류, 처리 중, 발송됨, 배달됨, 취소됨)
-            default: 'pending'
-        },
-        paymentMethod: { 
-            type: String, 
-            required: true,
-            enum: ['credit_card', 'paypal', 'bank_transfer'] // 결제 방법 (신용카드, 페이팔, 무통장 입금)
-        }, 
-        shippingAddress: { 
-            type: String, 
-            required: true 
-        }, // 배송 주소
-        shippingFee: { type: Number, required: true }, // 배송비
+        shipTo: { type: Object, required: true },
+        contact: { type: Object, required: true },
+        orderNum: { type: String },
+        items: [
+            {
+                productId: { type: mongoose.ObjectId, ref: Product, required: true },
+                price: { type: Number, required: true },
+                qty: { type: Number, required: true, default: 1 },
+                size: { type: String, required: true },
+            },
+        ],
+        // paymentMethod: {
+        //     type: String,
+        //     required: true,
+        //     enum: ['credit_card', 'paypal', 'bank_transfer'] // 결제 방법 (신용카드, 페이팔, 무통장 입금)
+        // },
     },
     { timestamps: true }
 );
@@ -32,6 +39,7 @@ const orderSchema = Schema(
 orderSchema.methods.toJSON = function () {
     const obj = this._doc;
     delete obj.__v;
+    delete obj.updatedAt;
     return obj;
 };
 
