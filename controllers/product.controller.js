7a8054153b2e10dfd1d84f512c538b69ac30327d
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-05 16:21:55 +0900

    feat: 모든 상품 조회 (+ 쿼리 조건 추가)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -41,12 +41,29 @@ productController.createProduct = async (req, res) => {
     }
 }
 
-// 모든 상품 조회
+// 모든 상품 조회 (+ 쿼리 조건 추가)
 productController.getProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
-        return res.status(200).json({ status: "success", products })
 
+        // body에서 가져온 쿼리(page, name)
+        const { page, name } = req.body
+
+        // 조건: name이라는 단어가 있을 경우, 대소문자 구분 없이
+        const condition = name ? { name: { $regex: name, options: "i" } } : {};
+
+        let query = Product.find(condition);
+        
+        // exec(): condition 찾을 때까지 기다렸다가 실행
+        const productList = await query.exec();
+
+        // if (name) {
+        //     const products = await Product.find({ name: { $regex: name, $options: "i" } })
+        // } else {
+        //     // 없으면 모두 조회
+        //     const products = await Product.find({});
+        // }
+
+        return res.status(200).json({ status: "success", productList })
     } catch (err) {
         res.status(500).json({ status: "fail", error: err, message: err.message });
     }
