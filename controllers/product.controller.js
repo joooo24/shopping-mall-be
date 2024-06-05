ef821ff3dd58f88f92e14aaa585196d35d041267
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-05 17:16:17 +0900

    feat: 상품 조회 (페이지 조건 추가)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,7 @@
+const { response } = require('express');
 const Product = require('../models/Product');
-const productController = {}
+const productController = {};
+const PAGE_SIZE = 5;
 
 // 상품 생성
 productController.createProduct = async (req, res) => {
@@ -41,29 +43,42 @@ productController.createProduct = async (req, res) => {
     }
 }
 
-// 모든 상품 조회 (+ 쿼리 조건 추가)
+// 상품 조회 (+ 쿼리 조건 추가)
 productController.getProducts = async (req, res) => {
     try {
 
         // body에서 가져온 쿼리(page, name)
         const { page, name } = req.body
 
-        // 조건: name이라는 단어가 있을 경우, 대소문자 구분 없이
+        // ### name이 있을 경우
+        // 조건: name이라는 쿼리가 있다면 -> name을 포함하기만 하면 되고, 대소문자 구분하지 않겠다.
         const condition = name ? { name: { $regex: name, options: "i" } } : {};
 
+        // condition에 따라 상품을 조회 함
         let query = Product.find(condition);
-        
-        // exec(): condition 찾을 때까지 기다렸다가 실행
-        const productList = await query.exec();
 
-        // if (name) {
-        //     const products = await Product.find({ name: { $regex: name, $options: "i" } })
-        // } else {
-        //     // 없으면 모두 조회
-        //     const products = await Product.find({});
-        // }
 
-        return res.status(200).json({ status: "success", productList })
+        // ### page가 있을 경우
+        // 현재 페이지에서 보여줄 첫 번째 항목을 결정하기 위한 로직
+        // '(현재 페이지 - 한 페이지에 보여주고싶은 개수) * 한 페이지에 보여주고싶은 개수'만큼 제외하고,
+        // 현재 페이지에 보여주고싶은 개수만큼만 보여주겠다
+        if (page) {
+            query.skip((page - PAGE_SIZE) * PAGE_SIZE).limit(PAGE_SIZE)
+        }
+
+        // 총 데이터 개수
+        const totalItemNum = await Product.find(condition).count();
+        response.totalItemNum = totalItemNum; // 응답 데이터 추가 -> response.totalItemNum
+
+        // 총 페이지 개수
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE)
+        response.totalPageNum = totalPageNum; // 응답 데이터 추가 -> response.totalPageNum
+
+        // condition 찾을 때까지 기다렸다가 실행
+        const productList = await query.exec(condition);
+        response.data = productList; // 응답 데이터 추가 -> response.data
+
+        return res.status(200).json({ status: "success",  productList, totalPageNum, totalItemNum })
     } catch (err) {
         res.status(500).json({ status: "fail", error: err, message: err.message });
     }
