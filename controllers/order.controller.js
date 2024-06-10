5e6948a34f7a454c2f08adba03ceb3ca8822403e
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-10 10:15:24 +0900

    feat: 주문

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -45,4 +45,102 @@ orderController.createOrder = async (req, res) => {
     }
 };
 
+// 주문 가져오기
+orderController.getOrder = async (req, res, next) => {
+    try {
+        // 요청에서 userId를 가져옴
+        const { userId } = req;
+
+        // userId로 사용자의 주문 목록을 찾고, items와 해당 items의 productId를 populate 함
+        // productId 필드에서 image와 name 속성만 선택하여 반환함
+        const orderList = await Order.find({ userId: userId }).populate({
+            path: "items",
+            populate: {
+                path: "productId",
+                model: "Product",
+                select: "image name",
+            },
+        });
+
+        // 사용자의 전체 주문 수를 계산함
+        const totalItemNum = await Order.find({ userId: userId }).count();
+
+        // 전체 페이지 수를 계산함 (페이지 크기는 PAGE_SIZE로 정의됨)
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+
+        // 성공적으로 주문 목록과 총 페이지 수를 응답으로 반환
+        res.status(200).json({ status: "success", data: orderList, totalPageNum });
+    } catch (error) {
+        // 에러가 발생한 경우 상태 코드 400과 함께 에러 메시지를 반환
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
+};
+
+// 주문 목록 가져오기
+orderController.getOrderList = async (req, res, next) => {
+    try {
+        // 요청 쿼리에서 페이지 번호와 주문 번호를 가져옴
+        const { page, ordernum } = req.query;
+
+        // 조건 객체를 초기화
+        let condition = {};
+        // 주문 번호가 쿼리에 존재하는 경우, 조건 객체에 정규 표현식 조건 추가
+        if (ordernum) {
+            condition = {
+                orderNum: { $regex: ordernum, $options: "i" },
+            };
+        }
+
+        // 조건에 맞는 주문 목록을 페이지네이션 적용하여 가져옴
+        const orderList = await Order.find(condition)
+            .populate("userId") // userId를 populate 함
+            .populate({
+                path: "items",
+                populate: {
+                    path: "productId",
+                    model: "Product",
+                    select: "image name",
+                },
+            })
+            .skip((page - 1) * PAGE_SIZE) // 페이지네이션 적용
+            .limit(PAGE_SIZE); // 페이지당 항목 수 제한
+        // 조건에 맞는 전체 주문 수를 계산
+        const totalItemNum = await Order.find(cond).count();
+
+        // 전체 페이지 수를 계산
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+
+        // 주문 목록과 총 페이지 수를 응답으로 반환
+        res.status(200).json({ status: "success", data: orderList, totalPageNum });
+    } catch (error) {
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
+};
+
+// 주문 상태 업데이트
+orderController.updateOrder = async (req, res, next) => {
+    try {
+        // 주문 ID
+        const { id: orderId } = req.params;
+        // 주문 상태
+        const { status } = req.body;
+
+        // 주문 ID로 주문을 찾아서 상태를 업데이트하고, 업데이트된 주문을 반환
+        const order = await Order.findByIdAndUpdate(
+            orderId,
+            { status: status },
+            { new: true } // 업데이트 후의 도큐먼트를 반환하도록 설정
+        );
+
+        // 주문을 찾지 못한 경우 에러를 발생시킴
+        if (!order) throw new Error("Can't find order");
+
+        // 성공적으로 업데이트된 주문을 응답으로 반환
+        res.status(200).json({ status: "success", data: order });
+    } catch (error) {
+        // 에러가 발생한 경우 상태 코드 400과 함께 에러 메시지를 반환
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
+};
+
 module.exports = orderController;
--- routes/order.api.js
+++ routes/order.api.js
@@ -8,4 +8,18 @@ const orderController = require("../controllers/order.controller");
 // 토큰 검증 -> 주문 생성
 router.post("/", authController.authenticate, orderController.createOrder);
 
+// 토큰 검증 -> 나의 주문 가져오기
+router.get("/me", authController.authenticate, orderController.getOrder);
+
+// 토큰 검증 -> 모든 주문 목록 가져오기 (관리자)
+router.get("/", authController.authenticate, orderController.getOrderList);
+
+// 토큰 검증 -> 관리자 확인 -> 주문 상태 변경
+router.put(
+    "/:id",
+    authController.authenticate,
+    authController.checkAdminPermission,
+    orderController.updateOrder
+);
+
 module.exports = router;
